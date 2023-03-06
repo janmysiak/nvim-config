e1c5e4c01b240ce3cc1a0bf1c448f6dc79fd96ab
janmysiak <jan.mysiak@noaignite.com>
2023-03-06 13:58:58 +0100

    update keymap

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -9,7 +9,7 @@ lsp.preset("recommended")
 lsp.ensure_installed({
 	"tsserver",
 	"eslint",
-	"sumneko_lua",
+	-- "sumneko_lua",
 	"rust_analyzer",
 	"marksman",
 	-- "gopls",
--- lua/user/keymap.lua
+++ lua/user/keymap.lua
@@ -2,26 +2,41 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- Buffers
-vim.keymap.set("n", "<leader>x", vim.cmd.bd)
-vim.keymap.set("n", "<leader><tab>", vim.cmd.bn)
-vim.keymap.set("n", "<leader><S-tab>", vim.cmd.bp)
+-- vim.keymap.set("n", "<leader>bc", ":bp | sp | bn | bd<CR>")
+vim.keymap.set("n", "<leader>bc", ":bd<CR>")
+vim.keymap.set("n", "<leader>bn", vim.cmd.bp)
+vim.keymap.set("n", "<leader>bp", vim.cmd.bn)
+-- vim.keymap.set("n", "<leader>bC", ":bufdo bd<CR>")
+vim.keymap.set("n", "<leader>bo", ":%bd|e#<CR>", {})
 
 -- Move lines
 vim.keymap.set("n", "<A-up>", ":m-2<CR>")
 vim.keymap.set("n", "<A-down>", ":m+1<CR>")
 vim.keymap.set("v", "<A-up>", ":m '<-2<CR>gv=gv")
 vim.keymap.set("v", "<A-down>", ":m '>+1<CR>gv=gv")
-vim.keymap.set("n", "<A-k>", ":m-2<CR>")
-vim.keymap.set("n", "<A-j>", ":m+1<CR>")
-vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv")
-vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv")
+vim.keymap.set("n", "<leader>k", ":m-2<CR>")
+vim.keymap.set("n", "<leader>j", ":m+1<CR>")
+vim.keymap.set("v", "<leader>k", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "<leader>j", ":m '>+1<CR>gv=gv")
 
 -- Scroll and center cursor
 vim.keymap.set({ "n", "v" }, "<S-up>", "<C-u>zz")
 vim.keymap.set({ "n", "v" }, "<S-down>", "<C-d>zz")
+vim.keymap.set({ "n", "v" }, "<C-k>", "<C-u>zz")
+vim.keymap.set({ "n", "v" }, "<C-j>", "<C-d>zz")
 
 -- Windows
 vim.keymap.set("n", "<leader>w", "<C-w>")
+
+-- Tabs
+vim.keymap.set("n", "<leader>tt", ":tabnew<CR>")
+vim.keymap.set("n", "<leader>tc", ":tabclose<CR>")
+vim.keymap.set("n", "<leader>tn", ":tabprev<CR>")
+vim.keymap.set("n", "<leader>tp", ":tabnext<CR>")
+vim.keymap.set("n", "<leader>tT", ":tab term<CR>")
+
+-- Terminal
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
 
 -- Toggle diff view
 vim.keymap.set({ "n", "v" }, "<leader>g", ":DiffviewOpen<CR>")
@@ -34,6 +49,7 @@ vim.keymap.set({ "n", "v" }, "<leader>c", ":Commentary<CR>")
 vim.keymap.set("n", "<leader>ld", vim.lsp.buf.definition)
 vim.keymap.set("n", "<leader>lk", vim.lsp.buf.hover)
 vim.keymap.set("n", "<leader>ls", vim.lsp.buf.code_action)
+vim.keymap.set({ "n", "v" }, "<leader>lr", vim.lsp.buf.rename)
 vim.keymap.set("n", "<leader>lj", vim.diagnostic.open_float)
 vim.keymap.set("n", "<leader>lh", vim.diagnostic.goto_prev)
 vim.keymap.set("n", "<leader>ll", vim.diagnostic.goto_next)
@@ -42,9 +58,11 @@ vim.keymap.set("n", "<leader>ll", vim.diagnostic.goto_next)
 local telescope_ok, telescope = pcall(require, "telescope.builtin")
 
 if telescope_ok then
-	vim.keymap.set("n", "<leader>f", telescope.find_files, {})
-	vim.keymap.set("n", "<leader>F", telescope.live_grep, {})
-	vim.keymap.set("n", "<leader>b", telescope.buffers, {})
+	vim.keymap.set("n", "<leader>ff", telescope.find_files, {})
+	vim.keymap.set("n", "<leader>fF", telescope.live_grep, {})
+	vim.keymap.set("n", "<leader>fs", telescope.live_grep, {})
+	vim.keymap.set("n", "<leader>fb", telescope.buffers, {})
+	vim.keymap.set("n", "<leader>bb", telescope.buffers, {})
 end
 
 -- Tree
